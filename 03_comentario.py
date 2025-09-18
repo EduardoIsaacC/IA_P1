3640deb41410a99763ac6a447ae7856edc521c8d
EduardoIsaacC <a23110190@live.ceti.mx>
2025-09-17 18:15:41 -0600

    Create 03_comentario.py

---
+++ 03_comentario.py
@@ -0,0 +1,34 @@
+print ("¡Hola MUndo!")
+
+#print ("esta linea no se ejecuta") (comentario)
+print("Esta linea si se ejecuta")
+
+#Division de secciones
+
+#comentario a la derecha
+print("hola") 
+print ("hola2") #Aqui esta el comentario
+print ("hola3")
+
+"""
+Este es un comentario
+multilínea de Python.
+Puedo coemntar lo que yo desee
+"""
+# Este código imprime los números del 1 al 10
+for i in range(1, 11):
+    print(i)
+"""
+# Este código no funcionara
+for i in range(10, 0, -1):
+    print(i)
+"""
+""" docstring del módulo """
+
+class Clase:
+    """ docstring de la clase """
+    def metodo(self):
+        """ docstring del método """
+
+    def funcion():
+        """ docstring de la función """
