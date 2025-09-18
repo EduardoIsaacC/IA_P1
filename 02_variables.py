7f3cbb0ce35daaf66c6601fb524df3744ed39569
EduardoIsaacC <a23110190@live.ceti.mx>
2025-09-17 18:08:02 -0600

    Create 02_variables.py

---
+++ 02_variables.py
@@ -0,0 +1,8 @@
+edad = 21; #Se evita el int
+edad = 30   # Reem´laza el valor anterior
+print (edad)
+
+numero_1, numero_2, numero_3 = 10, 20, 30
+print (numero_1, numero_2, numero_3)
+numero_1= "Hola" #La variable cambia a ser tipo texto
+print (numero_1)
